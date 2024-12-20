b07611e4a929595a9204e91c6d9339cb2654d3bc
wahsur <ngurahsurya736@gmail.com>
2024-12-20 10:01:56 +0800

    portofolio-gita

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px', 
+    },
+    extend: {
+      colors: {
+        primary: '#2563eb',
+        dark: '#0f172a',
+        secondary: '#64748b',
+      },
+      screens: {
+        '2xl': '1320px', 
+      },
+    },
+  },
+  plugins: [],
+};
+
+
